93ba2283b90820fbe42b3fb5be1f3cf466db7f66
Zayed Ahmed <zayed967@yahoo.com>
2025-12-06 08:11:31 -0500

    add update to l2 reugliraztion and test on breast cancer ds

--- src/model.py
+++ src/model.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 class LogisticRegression:
-    def __init__(self, n_features , lr=0.01, l2=0.0):
+    def __init__(self, n_features , lr=0.01, reg_lambda=0.001):
         # random weights
         self.weights = np.random.normal(0, 1, size=(n_features, 1))
         # bias
@@ -9,27 +9,28 @@ class LogisticRegression:
         # learning rate
         self.lr = lr
         # L2 regularization 
-        self.l2 = l2
+        self.reg_lambda = reg_lambda
 
     def forward(self, X):
         z = X @ self.weights + self.bias
-        sigmoid = 1 / (1 + np.exp(-z))
-        return z, sigmoid
+        y_hat = 1 / (1 + np.exp(-z))
+        return z, y_hat
 
     def loss(self, y, y_hat):
         eps = 1e-8
         y_hat = np.clip(y_hat, eps, 1 - eps)
-        loss = - (y * np.log(y_hat) + (1 - y) * np.log(1 - y_hat))
-        return loss.mean()
+        ce = -np.mean(y * np.log(y_hat) + (1 - y) * np.log(1 - y_hat))
+        l2 = (self.reg_lambda / (2 * len(y))) * np.sum(self.weights ** 2)
+
+        return ce + l2
 
     def backward(self, X, y, y_hat):
         error = y_hat - y          # (N, 1)
-        dW = X.T @ error           # (D, 1)
-        dB = np.sum(error)         # scalar
+        dW = (X.T @ error) / len(X) + (self.reg_lambda / len(X)) * self.weights         # (D, 1)
+        dB = np.mean(error)         # scalar
         return dW, dB
 
     def update(self, dW, dB):
-        dW += self.l2 * self.weights
         self.weights -= self.lr * dW 
         self.bias -= self.lr * dB
     
@@ -37,10 +38,12 @@ class LogisticRegression:
         preds = (y_hat >= 0.5).astype(int)
         return (preds == y).mean()
 
-    class StandardScaler:
-        def fit(self, X):
-            self.mean = X.mean(axis=0)
-            self.std = X.std(axis=0)
-        def transform(self, X):
-            return (X - self.mean) / (self.std + 1e-8)
+class StandardScaler:
+    def __init__(self):
+        pass
+    def fit(self, X):
+        self.mean = X.mean(axis=0)
+        self.std = X.std(axis=0)
+    def transform(self, X):
+        return (X - self.mean) / (self.std + 1e-8)
     
